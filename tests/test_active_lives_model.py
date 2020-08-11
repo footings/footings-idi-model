687663fd8cfc71289a3c1918b153222f9efceb60
Dustin Tindall <dustin.tindall@gmail.com>
2020-08-11 06:30:18 -0500

    Refactored active lives test to test against rop records.

--- tests/test_active_lives_model.py
+++ tests/test_active_lives_model.py
@@ -3,19 +3,17 @@ import pandas as pd
 from footings_idi_model.population_models import active_lives_model
 
 
-def add_rop_policies_to_extract(extract):
-    extract_rop = extract.copy()
-    extract_rop["COVERAGE_ID"] = "rop"
-    return pd.concat([extract, extract_rop], axis=0).sort_values(by=["POLICY_ID"], ignore_index=True)
-
-
 def test_active_lives_model():
-    extract = pd.read_csv(
-        "./tests/data/active-lives-sample.csv",
+    base_extract = pd.read_csv(
+        "./tests/data/active-lives-sample-base.csv",
         parse_dates=["BIRTH_DT", "POLICY_START_DT", "PREMIUM_PAY_TO_DT", "POLICY_END_DT"],
     )
+    rider_extract = pd.read_csv(
+        "./tests/data/active-lives-sample-riders.csv",
+    )
     time0, projected, errors = active_lives_model(
-        extract=extract,
+        base_extract=base_extract,
+        rider_extract=rider_extract,
         valuation_dt=pd.Timestamp("2020-03-31"),
         assumption_set="stat",
         net_benefit_method="NLP",
