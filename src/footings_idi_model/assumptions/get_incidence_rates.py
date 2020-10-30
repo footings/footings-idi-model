0432f6432a9467590b962de251fd5b2cc350484f
Dustin Tindall <dustin.tindall@gmail.com>
2020-10-30 11:46:42 -0500

    ALR incidence rate functions for stat/gaap.

---
+++ src/footings_idi_model/assumptions/get_incidence_rates.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from footings import dispatch_function
+
+from .stat_gaap.incidence import _stat_gaap_incidence
+
+
+@dispatch_function(key_parameters=("assumption_set",))
+def get_incidence_rates(assumption_set, model_object):
+    """ """
+    msg = "No registered function based on passed paramters and no default function."
+    raise NotImplementedError(msg)
+
+
+@get_incidence_rates.register(assumption_set="stat")
+def _(model_object):
+    return _stat_gaap_incidence(
+        idi_contract=model_object.idi_contract,
+        idi_occupation_class=model_object.idi_occupation_class,
+        idi_market=model_object.idi_market,
+        idi_benefit_period=model_object.idi_benefit_period,
+        tobacco_usage=model_object.tobacco_usage,
+        elimination_period=model_object.elimination_period,
+        gender=model_object.gender,
+    )
+
+
+@get_incidence_rates.register(assumption_set="gaap")
+def _(model_object):
+    return _stat_gaap_incidence(
+        idi_contract=model_object.idi_contract,
+        idi_occupation_class=model_object.idi_occupation_class,
+        idi_market=model_object.idi_market,
+        idi_benefit_period=model_object.idi_benefit_period,
+        tobacco_usage=model_object.tobacco_usage,
+        elimination_period=model_object.elimination_period,
+        gender=model_object.gender,
+    )
+
+
+@get_incidence_rates.register(assumption_set="best-estimate")
+def _(model_object):
+    raise NotImplementedError("Best estimate assumptions are not implemented yet.")
--- src/footings_idi_model/assumptions/stat_gaap/incidence/__init__.py
+++ src/footings_idi_model/assumptions/stat_gaap/incidence/__init__.py
@@ -1,80 +1,177 @@
-"""Prepare incidence assumptions"""
-
 import os
 from functools import lru_cache
 import json
 
 import pandas as pd
 
+from footings.tools import once
+
 directory, filename = os.path.split(__file__)
 
-# benefit period modifier
-benefit_period_file = os.path.join(directory, "2013-idi-benefit-period-modifier.csv")
+
+@once
+def read_base_incidence():
+    file = os.path.join(directory, "2013-idi-base-incidence.csv")
+    dtypes = {
+        "TYPE": "category",
+        "IDI_OCCUPATION_CLASS": "category",
+        "GENDER": "category",
+    }
+    return pd.read_csv(file, dtype=dtypes)
 
 
-@lru_cache(maxsize=2)
-def get_benefit_period_modifier(file):
-    """Get benefit period modifier"""
-    return pd.read_csv(file)
+@lru_cache(256)
+def get_base_incidence(idi_contract, idi_occupation_class, gender, elimination_period):
+    tbl = read_base_incidence()
+    incidence_cols = [
+        "IDI_OCCUPATION_CLASS",
+        "GENDER",
+        "ELIMINATION_PERIOD",
+        "AGE_ATTAINED",
+    ]
+    if idi_contract == "AO":
+        tbl = tbl[tbl.TYPE == "Acc"]
+    elif idi_contract == "SO":
+        tbl = tbl[tbl.TYPE == "Sck"]
+    else:
+        tbl = tbl.groupby(incidence_cols, as_index=False).sum()
+    return tbl[
+        (tbl.IDI_OCCUPATION_CLASS == idi_occupation_class)
+        & (tbl.GENDER == gender)
+        & (tbl.ELIMINATION_PERIOD == elimination_period)
+    ][incidence_cols + ["INCIDENCES"]]
 
 
-# contract modifier
-contract_file = os.path.join(directory, "2013-idi-contract-modifier.csv")
+@once
+def read_benefit_period_modifiers():
+    file = os.path.join(directory, "2013-idi-benefit-period-modifier.csv")
+    dtypes = {
+        "IDI_OCCUPATION_CLASS": "category",
+        "IDI_BENEFIT_PERIOD": "category",
+    }
+    return pd.read_csv(file, dtype=dtypes)
 
 
-@lru_cache(maxsize=2)
-def get_contract_modifier(file):
-    """Get contract modifier"""
-    return pd.read_csv(file)
+@lru_cache(maxsize=512)
+def get_benefit_period_modifier(
+    idi_occupation_class, idi_benefit_period, elimination_period
+):
+    tbl = read_benefit_period_modifiers()
+    return tbl[
+        (tbl.IDI_OCCUPATION_CLASS == idi_occupation_class)
+        & (tbl.IDI_BENEFIT_PERIOD == idi_benefit_period)
+        & (tbl.ELIMINATION_PERIOD == elimination_period)
+    ]["BENEFIT_PERIOD_MODIFIER"].iat[0]
 
 
-# market modifier
-market_file = os.path.join(directory, "2013-idi-market-modifier.csv")
-
-
-@lru_cache(maxsize=2)
-def get_market_modifier(file):
-    """Get market modifier"""
-    return pd.read_csv(file)
-
-
-# tobacco modifier
-tobacco_file = os.path.join(directory, "2013-idi-tobacco-modifier.csv")
-
-
-@lru_cache(maxsize=2)
-def get_tobacco_modifier(file):
-    """Get tobacco modifier"""
-    return pd.read_csv(file)
-
-
-# base incidence
-incidence_file = os.path.join(directory, "2013-idi-base-incidence.csv")
+@once
+def read_contract_modifiers():
+    file = os.path.join(directory, "2013-idi-contract-modifier.csv")
+    dtypes = {"IDI_CONTRACT": "category"}
+    return pd.read_csv(file, dtype=dtypes)
 
 
 @lru_cache(maxsize=4)
-def get_base_incidence(file, cause):
-    """Get base incidence"""
-    tbl = pd.read_csv(file)
-    if cause == "combined":
-        cols = ["IDI_OCCUPATION_CLASS", "GENDER", "ELIMINATION_PERIOD", "AGE_ATTAINED"]
-        return tbl.groupby(cols).sum()
-    if cause == "accident":
-        return tbl[tbl.TYPE == "Acc"]
-    if cause == "sickness":
-        return tbl[tbl.TYPE == "Sck"]
-    raise ValueError(f"The cause [{cause}] is not recognized. See documentation.")
+def get_contract_modifier(idi_contract):
+    tbl = read_contract_modifiers()
+    return tbl[tbl.IDI_CONTRACT == idi_contract]["CONTRACT_MODIFIER"].iat[0]
 
 
-# margin
-margin_file = os.path.join(directory, "margin.json")
+@once
+def read_market_modifiers():
+    file = os.path.join(directory, "2013-idi-market-modifier.csv")
+    dtypes = {"IDI_MARKET": "category"}
+    return pd.read_csv(file, dtype=dtypes)
 
 
-@lru_cache(maxsize=2)
-def get_margin(file):
-    """Get incidence margin"""
+@lru_cache(maxsize=4)
+def get_market_modifier(idi_market):
+    tbl = read_market_modifiers()
+    return tbl[tbl.IDI_MARKET == idi_market]["MARKET_MODIFIER"].iat[0]
+
+
+@once
+def read_tobacco_modifiers():
+    file = os.path.join(directory, "2013-idi-tobacco-modifier.csv")
+    dtypes = {
+        "IDI_OCCUPATION_CLASS": "category",
+        "GENDER": "category",
+        "TOBACCO_USAGE": "category",
+    }
+    return pd.read_csv(file, dtype=dtypes)
+
+
+@lru_cache(maxsize=256)
+def get_tobacco_modifier(idi_occupation_class, gender, tobacco_usage):
+    tbl = read_tobacco_modifiers()
+    return tbl[
+        (tbl.IDI_OCCUPATION_CLASS == idi_occupation_class)
+        & (tbl.GENDER == gender)
+        & (tbl.TOBACCO_USAGE == tobacco_usage)
+    ]["TOBACCO_MODIFIER"].iat[0]
+
+
+@once
+def get_margin_adjustment():
+    file = os.path.join(directory, "margin.json")
     with open(file, "r") as f:
         margin_temp = json.load(f)
-    margin = 1 + margin_temp["DURATION_1+"]
-    tbl = pd.DataFrame({"DURATION_YEAR": range(1, 101)}).assign(MARGIN_ADJUSTMENT=margin)
-    return tbl
+    return 1 + margin_temp["DURATION_1+"]
+
+
+def _stat_gaap_incidence(
+    idi_contract,
+    idi_occupation_class,
+    idi_market,
+    idi_benefit_period,
+    tobacco_usage,
+    elimination_period,
+    gender,
+):
+
+    prod_cols = [
+        "INCIDENCES",
+        "BENEFIT_PERIOD_MODIFIER",
+        "CONTRACT_MODIFIER",
+        "MARKET_MODIFIER",
+        "TOBACCO_MODIFIER",
+        "MARGIN_ADJUSTMENT",
+    ]
+
+    frame = get_base_incidence(
+        idi_contract, idi_occupation_class, gender, elimination_period
+    ).reset_index()
+    frame["IDI_CONTRACT"] = idi_contract
+    frame["IDI_BENEFIT_PERIOD"] = idi_benefit_period
+    frame["IDI_MARKET"] = idi_market
+    frame["TOBACCO_USAGE"] = tobacco_usage
+    frame["BENEFIT_PERIOD_MODIFIER"] = get_benefit_period_modifier(
+        idi_occupation_class, idi_benefit_period, elimination_period
+    )
+    frame["CONTRACT_MODIFIER"] = get_contract_modifier(idi_contract)
+    frame["MARKET_MODIFIER"] = get_market_modifier(idi_market)
+    frame["TOBACCO_MODIFIER"] = get_tobacco_modifier(
+        idi_occupation_class, gender, tobacco_usage
+    )
+    frame["MARGIN_ADJUSTMENT"] = get_margin_adjustment()
+    frame["INCIDENCE_RATE"] = frame[prod_cols].prod(axis=1).div(1000)
+
+    return frame[
+        [
+            "IDI_BENEFIT_PERIOD",
+            "IDI_CONTRACT",
+            "IDI_MARKET",
+            "IDI_OCCUPATION_CLASS",
+            "TOBACCO_USAGE",
+            "ELIMINATION_PERIOD",
+            "GENDER",
+            "AGE_ATTAINED",
+            "INCIDENCES",
+            "BENEFIT_PERIOD_MODIFIER",
+            "CONTRACT_MODIFIER",
+            "MARKET_MODIFIER",
+            "TOBACCO_MODIFIER",
+            "MARGIN_ADJUSTMENT",
+            "INCIDENCE_RATE",
+        ]
+    ]
