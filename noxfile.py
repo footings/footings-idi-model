6204d8e21d212bd54e8c768d7e3208025c038e4a
Dustin Tindall <dustin.tindall@gmail.com>
2021-01-31 13:09:46 -0600

    Update to noxfile.

--- noxfile.py
+++ noxfile.py
@@ -2,40 +2,27 @@ import os
 import shutil
 import nox
 
-PYTHON_TEST_VERSIONS = ["3.6", "3.7", "3.8"]
 
-
-@nox.session(python=PYTHON_TEST_VERSIONS, venv_backend="conda")
-def tests(session):
-    session.run(
-        "conda",
-        "env",
-        "update",
-        "--prefix",
-        session.virtualenv.location,
-        "--file",
-        "environments/environment-dev.yml",
-        # options
-        silent=False,
-    )
-    session.install("--no-deps", "footings")
-    session.install("-e", ".", "--no-deps")
+@nox.session(venv_backend="none")
+def run_tests(session):
+    session.run("pip", "install", "--no-deps", ".")
+    session.run("pip", "show", "footings_idi_model")
     session.run("pytest")
 
 
-@nox.session(python="3.7", venv_backend="none")
-def docs(session):
-    if os.path.exists("./docs/_build/"):
-        shutil.rmtree("./docs/_build/")
-    if os.path.exists("./docs/jupyter_execute/"):
-        shutil.rmtree("./docs/jupyter_execute/")
-    os.mkdir("./docs/_build/")
-    session.install(".")
+@nox.session(venv_backend="none")
+def create_docs(session):
+    if os.path.exists("./docs/jupyter_execute"):
+        shutil.rmtree("./docs/jupyter_execute")
+    if os.path.exists("./docs/_build"):
+        shutil.rmtree("./docs/_build")
+    session.run("pip", "install", "--no-deps", ".")
+    session.run("pip", "show", "footings_idi_model")
     session.run("sphinx-build", "-E", "-v", "-b", "html", "docs", "docs/_build")
 
 
-@nox.session(python="3.7", venv_backend="none")
-def changelog(session):
+@nox.session(venv_backend="none")
+def update_changelog(session):
     session.run(
         "auto-changelog",
         "--output",
