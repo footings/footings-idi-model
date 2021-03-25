6fd034a9d4dd72d72c244b5c1c01f098f69e1547
Dustin Tindall <dustin.tindall@gmail.com>
2021-03-25 06:25:47 -0500

    First attempt at main.

--- src/footings_idi_model/__main__.py
+++ src/footings_idi_model/__main__.py
@@ -1,11 +1,67 @@
 import click
 
 
-@click.command()
-def main():
+@click.group()
+def cli():
     """CLI tool to run footings IDI model."""
     pass
 
 
+@cli.command()
+@cli.option("--all", default=False, help="Run all scenarios.")
+@cli.argument("extract_base")
+@cli.argument("extract_riders")
+@cli.argument("valuation_dt")
+@cli.argument("assumption_set")
+@cli.argument("scenarios", nargs=-1)
+def run_active_life_valuation(
+    all, extract_base, extract_riders, valuation_dt, assumption_set, scenarios
+):
+    """Run valuation model."""
+    return valuation_dt
+
+
+@cli.command()
+@cli.option("--all", default=False, help="Run all scenarios.")
+@cli.argument("extract_base")
+@cli.argument("extract_riders")
+@cli.argument("valuation_dt")
+@cli.argument("assumption_set")
+@cli.argument("scenarios", nargs=-1)
+def run_active_life_projection(
+    all, extract_base, extract_riders, valuation_dt, assumption_set, scenarios
+):
+    """Run projection model."""
+    return valuation_dt
+
+
+@cli.command()
+@cli.option("--all", default=False, help="Run all scenarios.")
+@cli.argument("extract_base")
+@cli.argument("extract_riders")
+@cli.argument("valuation_dt")
+@cli.argument("assumption_set")
+@cli.argument("scenarios", nargs=-1)
+def run_disabled_life_valuation(
+    all, extract_base, extract_riders, valuation_dt, assumption_set, scenarios
+):
+    """Run valuation model."""
+    return valuation_dt
+
+
+@cli.command()
+@cli.option("--all", default=False, help="Run all scenarios.")
+@cli.argument("extract_base")
+@cli.argument("extract_riders")
+@cli.argument("valuation_dt")
+@cli.argument("assumption_set")
+@cli.argument("scenarios", nargs=-1)
+def run_disabled_life_projection(
+    all, extract_base, extract_riders, valuation_dt, assumption_set, scenarios
+):
+    """Run projection model."""
+    return valuation_dt
+
+
 if __name__ == "__main__":
-    main()
+    cli()
